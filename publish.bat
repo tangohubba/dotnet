dde6c69c67951fb05d5b24afc09a279230e4eb92
tangohubba <lorrettus@hotmail.com>
2019-02-04 10:29:12 -0500

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -1,14 +1,15 @@
+@echo off
 cd  mvc
 dotnet publish -o publish
 
-#replace dockeruser1212 with your own docker hub id 
+REM #replace dockeruser1212 with your own docker hub id 
 docker build -t dockeruser1212/mvc .
 docker push dockeruser1212/mvc
 
 cd ..\restapi
 dotnet publish -o publish
 
-#replace dockeruser1212 with your own docker hub id 
+REM #replace dockeruser1212 with your own docker hub id 
 docker build -t dockeruser1212/restapi .
 docker push dockeruser1212/restapi
 
